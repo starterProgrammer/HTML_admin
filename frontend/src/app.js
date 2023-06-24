3370cee9d0526a3337449a695ce8c01833032694
starterProgrammer <ali.talaee.engeneer@gmail.com>
2023-06-24 16:47:53 +0330

    implement dark and light theme

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,4 +5,13 @@ document.getElementById('close-btn').addEventListener('click', () => {
     const asideMenu = document.getElementById('side-menu')
     asideMenu.style.display = "none"
 })
+const toggler = document.querySelector('.theme-toggler')
 
+toggler.addEventListener('click', () => {
+
+    toggler.children[0].classList.toggle('active')
+    document.querySelector('span:nth-child(2)').classList.toggle('active')
+
+    document.body.classList.toggle('dark-theme-varriables')
+
+})
